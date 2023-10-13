64a42c3cb2103ded6ed918abb8b2582dcc09065b
aditirao7 <aditirao725@gmail.com>
2023-10-13 23:10:19 +0530

    Add this github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,16 @@ import { Section } from "./Layout";
 const Footer = () => {
   return (
     <Section id="footer" padding="py-5" gap="gap-12" bg="bg-gray-950">
-      2023 | Developed with ❤️️ by Aditi Rao
+      <span>
+        2023 |{"  "}
+        <a
+          className="underline underline-offset-4"
+          href="https://github.com/aditirao7/aditirao7.github.io"
+        >
+          Developed
+        </a>{" "}
+        with ❤️️ by Aditi Rao
+      </span>
     </Section>
   );
 };
