afba8e73a94e45805d9a9fc9ad19ed454c16f0f0
aditirao7 <aditirao725@gmail.com>
2023-10-18 19:26:42 +0530

    testing 2.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,25 @@ import "../css/output.css";
 const IndexPage = () => {
   return (
     <main>
+      <Script>
+        {`
+          var _paq = (window._paq = window._paq || []);
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["trackPageView"]);
+          _paq.push(["enableLinkTracking"]);
+          (function () {
+            var u = "https://aditirao7.matomo.cloud/";
+            _paq.push(["setTrackerUrl", u + "matomo.php"]);
+            _paq.push(["setSiteId", "1"]);
+            var d = document,
+              g = d.createElement("script"),
+              s = d.getElementsByTagName("script")[0];
+            g.async = true;
+            g.src = "//cdn.matomo.cloud/aditirao7.matomo.cloud/matomo.js";
+            s.parentNode.insertBefore(g, s);
+          })();
+        `}
+      </Script>
       <Navigation></Navigation>
       <Intro></Intro>
       <About></About>
